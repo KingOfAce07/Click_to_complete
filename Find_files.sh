4e2b7aa9a1f26d6cdb60ef584e334a8f056089e8
KingOfAce07 <39474228+KingOfAce07@users.noreply.github.com>
2020-12-14 12:18:17 -0600

    Update Find_files.sh

--- Find_files.sh
+++ Find_files.sh
@@ -3,7 +3,7 @@
 # change the test folder = /dir/listofxls
 files="$(ls test | grep .csv)" 
 for line in $files ; do
-    cp $line $line.txt
+    cp $line $line
     #xlsx2csv Server_Inventory_Linux.xlsx Server_Inventory_Linux.csv
     #xlsx2csv $line $line.csv # uncomment to enable xls2csv function
 done
